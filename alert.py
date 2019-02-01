0e86a2b835f44364103f310af0636fb22e8e1548
atlastsciencefair <47194713+atlastsciencefair@users.noreply.github.com>
2019-01-31 20:43:06 -0500

    Add files via upload

---
+++ alert.py
@@ -0,0 +1,50 @@
+import http.client, urllib
+import os
+import glob
+import time
+ 
+os.system('modprobe w1-gpio')
+os.system('modprobe w1-therm')
+ 
+base_dir = '/sys/bus/w1/devices/'
+device_folder = glob.glob(base_dir + '28*')[0]
+device_file = device_folder + '/w1_slave'
+ 
+def read_temp_raw():
+    f = open(device_file, 'r')
+    lines = f.readlines()
+    f.close()
+    return lines
+ 
+def read_temp():
+    lines = read_temp_raw()
+    while lines[0].strip()[-3:] != 'YES':
+        time.sleep(0.2)
+        lines = read_temp_raw()
+    equals_pos = lines[1].find('t=')
+    if equals_pos != -1:
+        temp_string = lines[1][equals_pos+2:]
+        temp_c = float(temp_string) / 1000.0
+        return temp_c
+	
+
+
+car_on = True
+temp = read_temp 
+i = 0
+while car_on == True:
+    i += 1
+	if read_temp() >= 0: 
+	    conn = http.client.HTTPSConnection("api.pushover.net:443")
+	    conn.request("POST", "/1/messages.json",
+		    urllib.parse.urlencode({
+		    "token": "aajaiutjvx1fwdqy15g2nqcp6av8dt",
+		    "user": "usqmx1mv4pams23f4nq8igh5a7okzf",
+		    "message": "Your vehicle has reached a DANGEROUS temperature. Please return to your vehicle",
+		    }), { "Content-type": "application/x-www-form-urlencoded" })
+	    conn.getresponse()
+	if i >= 5
+	    spam = input("would you like to continue to spam: ")
+	    if spam.rstrip() == "yes":
+		exit()
+	    
